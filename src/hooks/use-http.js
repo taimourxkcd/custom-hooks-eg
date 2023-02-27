0a71e977641ab5cd76c6da8ce2c1cd1fd2d61f35
taimourz <taimour.rkt@gmail.com>
2023-02-27 19:09:31 +0500

    adjusted custom hook fixed bugs

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useCallback, useState  } from "react";
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async (taskText) => {
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(requestConfig.url, {
-        method: requestConfig.method ? requestConfig.method : 'GET',
+        method: requestConfig.method ? requestConfig.method : "GET",
         headers: requestConfig.headers ? requestConfig.headers : {},
         body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
@@ -20,19 +20,11 @@ const useHttp = (requestConfig, applyData) => {
 
       const data = await response.json();
       applyData(data);
-
-      //   const loadedTasks = [];
-
-      //   for (const taskKey in data) {
-      //     loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      //   }
-
-      //   setTasks(loadedTasks);
     } catch (err) {
       setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
-  };
+  }, []);
 
   return {
     isLoading: isLoading,
